edb987dcf474193563ee65f2c021820ca0d632e8
RodrigoTeodoro01 <rodrigosouza01@hotmail.com>
2025-07-18 19:13:21 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('suggestionForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    // Simula envio e exibe uma mensagem
-    document.getElementById('responseMessage').textContent = 'Obrigado por sua sugestão!';
-    this.reset();
-});
